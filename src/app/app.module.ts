c2b60c412a95d171426ee9f2282d37b0256928b7
bartlomiejwitek <56968287+bartlomiejwitek@users.noreply.github.com>
2021-04-04 18:18:03 +0200

    Improved componen layout and added login, register routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PageHomeComponent } from './page-home/page-home.component';
-import { PageLoginComponent } from './page-login/page-login.component';
-import { PageEmployeePanelComponent } from './page-employee-panel/page-employee-panel.component';
-import { PageAdminPanelComponent } from './page-admin-panel/page-admin-panel.component';
-import { PageUserPanelComponent } from './page-user-panel/page-user-panel.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { PageHomeComponent } from './public/page-home/page-home.component';
+import { PageLoginComponent } from './public/page-login/page-login.component';
+import { PageEmployeePanelComponent } from './private/page-employee-panel/page-employee-panel.component';
+import { PageAdminPanelComponent } from './private/page-admin-panel/page-admin-panel.component';
+import { PageUserPanelComponent } from './private/page-user-panel/page-user-panel.component';
+import { PageRegisterComponent } from './public/page-register/page-register.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,19 @@ import { PageUserPanelComponent } from './page-user-panel/page-user-panel.compon
     PageLoginComponent,
     PageEmployeePanelComponent,
     PageAdminPanelComponent,
-    PageUserPanelComponent
+    PageUserPanelComponent,
+    PageRegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: PageHomeComponent },
+      { path: 'login', component: PageLoginComponent },
+      { path: 'register', component: PageRegisterComponent},
+      { path: 'private/employee-panel', component: PageEmployeePanelComponent},
+      { path: 'private/admin-panel', component: PageAdminPanelComponent},
+      { path: 'private/user-panel', component: PageUserPanelComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
